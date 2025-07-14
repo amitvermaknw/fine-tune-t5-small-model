f6ac1263c4c7c592a828e2aef9da092c78417876
Amit Verma <amitfromus@gmail.com>
2025-07-13 18:40:33 -0700

    fixed issue

--- src/const.py
+++ src/const.py
@@ -1,7 +1,6 @@
-class Constant:
-    def __init__(self):
-        self.model_name="t5-small"
-        self.max_input_len = 512
-        self.max_output_len= 100
 
+class Constant:
+    MODEL_NAME="t5-small"
+    MAX_INPUT_LEN = 512
+    MAX_OUTPUT_LEN= 100
         
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -6,26 +6,27 @@ class Tokenizer:
     def __init__(self):
         self.model = None
         self.tokenizer = None
-
+        self.tokenized_train_dataset = None
+        self.tokenized_validation_dataset =None
 
     def create_token(self, model_name:str):
         self.tokenizer = T5Tokenizer.from_pretrained(model_name)
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.model = T5ForConditionalGeneration.from_pretrained(model_name)
-
+        return self
 
     def add_prefix(self, articles):
         inputs = [f"summarize: {document}" for document in articles["article"]]
         model_token_input = self.tokenizer(
             inputs, 
-            max_length=Constant().max_input_len, 
+            max_length=Constant.MAX_INPUT_LEN, 
             truncation=True,
             padding="max_length",
             return_tensors="pt")
          
         labels = self.tokenizer(
             text_target=articles["highlights"], 
-            max_length=Constant().max_output_len, 
+            max_length=Constant.MAX_OUTPUT_LEN, 
             truncation=True,
             padding="max_length",
             return_tensors="pt")
@@ -33,12 +34,16 @@ class Tokenizer:
         model_token_input["labels"] = labels["input_ids"]
         return model_token_input
     
-    @staticmethod
-    def processing_dataset():
+    def processing_dataset(self):
+        tokenizer_class = Tokenizer()
+        self.ct = tokenizer_class.create_token(Constant.MODEL_NAME)
+
         print("Preprocessing the dataset")
         dataset = LoadDataset().load_data()
         train_dataset = dataset["train"]
         validation_dataset = dataset["validation"]
-        tokenized_train_dataset = train_dataset.map(Tokenizer().add_prefix, batched=True, remove_columns=["articles", "highlights", "id"])
-        tokenized_validation_dataset =validation_dataset.map(Tokenizer().add_prefix, batched=True, remove_columns=["articles", "highlights", "id"])
+        self.tokenized_train_dataset = train_dataset.map(tokenizer_class.add_prefix, batched=True, remove_columns=["article", "highlights", "id"])
+        self.tokenized_validation_dataset =validation_dataset.map(tokenizer_class.add_prefix, batched=True, remove_columns=["article", "highlights", "id"])
         print("Dataset processing completed")
+
+        return self
