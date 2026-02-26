68973750f318471af7f3f47925b30073485342d6
Amit Verma <amitfromus@gmail.com>
2026-02-25 22:17:01 -0700

    updated the dataset to train on less data for testing

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -5,5 +5,9 @@ import evaluate
 class LoadDataset:
     def load_data(self):
         dataset = load_dataset("cnn_dailymail", "3.0.0")
-        # print("Dataset loaded", dataset)
+        # For testing on vertex ai using the 1000 dataset for training
+        # And 200 dataset for val
+        dataset["train"] = dataset["train"].shuffle(seed=42).select(range(1000))
+        dataset["validation"] = dataset["validation"].shuffle(seed=42).select(range(1000))
+
         return dataset
